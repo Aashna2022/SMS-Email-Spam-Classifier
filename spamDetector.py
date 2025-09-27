76aef522750f2ea78810a3e78d997c620e788e8a
Aashna Singh <singhaashna2022@gmail.com>
2025-09-27 07:57:24 +0530

    Add files via upload

---
+++ spamDetector.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pickle
+
+model = pickle.load(open("spam.pkl", "rb"))
+cv = pickle.load(open("vectorizer.pkl", "rb"))
+
+
+def main():
+    st.title("Email Spam Classifier App")
+    msg = st.text_input("Enter a text:")
+    if st.button("Predict"):
+        data = [msg]
+        vect = cv.transform(data).toarray()
+        prediction = model.predict(vect)
+        result = prediction[0]
+        if result == 1:
+            st.error("This is a spam mail")
+        else:
+            st.success("This is a ham mail")
+
+
+main()
